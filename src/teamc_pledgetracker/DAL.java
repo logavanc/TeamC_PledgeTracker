31878e46e6a42860173db01b82d0725262b2b79f
leverett <leverett@leverett-5697>
2014-04-14 17:00:53 -0700

    Initial Commit - 03/31/2014 11:16 am

--- src/teamc_pledgetracker/DAL.java
+++ src/teamc_pledgetracker/DAL.java
@@ -14,7 +14,7 @@ public class DAL {
     private boolean bClosed = false;
     
     public DAL (){
-        init(StorageType.FILE); //Default Week 4
+        init(StorageType.DATABASE); //Default Week 4
     }
     public DAL (StorageType storageType) {
         init(storageType);
--- src/teamc_pledgetracker/DatabaseDAL.java
+++ src/teamc_pledgetracker/DatabaseDAL.java
@@ -1,31 +1,94 @@
 package teamc_pledgetracker;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.*;
 
 /**
  *
  * @author leverett
  */
 public class DatabaseDAL implements IDAL {
+    private static final String url = "jdbc:mysql://sql3.freemysqlhosting.net:3306/";
+    private static final String dbName = "sql336726";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String userName = "sql336726";
+    private static final String password = "eD6*iS6*";
+    private Connection conn;
 
     @Override
     public boolean init() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean bRet = true;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            try {
+                conn = DriverManager.getConnection(url+dbName,userName,password);
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            bRet = false;
+        }
+        return bRet;
     }
 
     @Override
     public boolean saveData(Pledge pledge) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean bRet = true;
+        try {
+            PreparedStatement st = conn.prepareStatement("INSERT into Pledges VALUES(?,?,?)");
+            //Make vales SQL safe
+            st.setString(1, pledge.getName());
+            st.setString(2, pledge.getCharity());
+            st.setInt(3, pledge.getPledgeamt());
+            
+            int iRetVal = st.executeUpdate();
+            if (iRetVal != 1) {
+                bRet = false;
+            }
+            //cleanup
+            st.close();
+            st = null;
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            bRet = false;
+        }
+        return bRet;
     }
 
     @Override
     public List<Pledge> getData() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Pledge> Pledges = new ArrayList<>();        
+        try {
+            Statement st = conn.createStatement();
+            ResultSet res = st.executeQuery("SELECT * FROM Pledges");
+            while (res.next()) {
+                Pledges.add(new Pledge(res.getString("name"), res.getString("charity"), res.getInt("pledge")));
+            }
+            //cleanup
+            res.close();
+            res = null;
+            st.close();
+            st = null;
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Pledges;
     }
 
     @Override
     public void close() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (conn != null) {
+            try {
+                //cleanup
+                conn.close();
+                conn = null;
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
 }
